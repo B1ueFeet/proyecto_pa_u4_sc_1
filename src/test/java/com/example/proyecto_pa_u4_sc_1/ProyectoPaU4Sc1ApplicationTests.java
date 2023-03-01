8cc696fca24a85aa1e7e30d32977742c4733216e
BlueFeet <b1uefeet.dev@gmail.com>
2023-02-28 19:42:44 -0500

    Taller 36: MVC

---
+++ src/test/java/com/example/proyecto_pa_u4_sc_1/ProyectoPaU4Sc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_pa_u4_sc_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPaU4Sc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
